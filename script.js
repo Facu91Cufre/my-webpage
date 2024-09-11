d9d03314010054bc3e0273a1c0bd3eb4cbf04c7b
Facu91Cufre <facundocufre91@gmail.com>
2024-09-11 16:47:26 -0300

    grid for certificates and projects

---
+++ script.js
@@ -0,0 +1,4 @@
+const modalImg = document.getElementById("modal-img");
+const closeBtn = document.getElementById("close-modal-btn");
+const modalOverlay = document.querySelector(".modal-overlay");
+
